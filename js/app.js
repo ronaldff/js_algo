fa8490f27c118a80a90e3335e3a0352ebcada2ca
piyush shyam <piyush.d.shyam@gmail.com>
2018-10-03 13:06:31 +0530

    modified one more time

--- js/app.js
+++ js/app.js
@@ -18,18 +18,27 @@ xml.onload = function(){
                       <div class="likes_comment clearfix">
                         <div id="comments" class="mr-2 ">
                           <div class="viewsDiv">
-                           <i class="fa fa-eye views" aria-hidden="true" title="views"></i>
-                           <span class="viewsData">${jsonData.views}</span>
+                            <a href = "#" id="viewsanchor"
+                              <i class="fa fa-eye views" aria-hidden="true" title="views">
+                                <span class="views-counter">${jsonData.views}</span>
+                              </i>
+                            </a>
                           </div>
                           
                           <div class="commentsDiv">
-                            <i class="fa fa-comments comments" aria-hidden="true" title="comments"></i> 
-                           <span class="commentsData">${jsonData.comments}</span>
+                            <a href="#" id="commentsanchor"
+                              <i class="fa fa-comments comments" aria-hidden="true" title="comments">
+                                <span class="comments-counter">${jsonData.comments}</span>
+                              </i> 
+                            </a>
                           </div>
                           
                           <div class="likesDiv">
-                            <i class="fa fa-thumbs-up likes" title="likes" aria-hidden="true"></i>
-                           <span class="likesData">${jsonData.likes}</span>
+                            <a href = "href" id="likesanchor"
+                              <i class="fa fa-thumbs-up likes" title="likes" aria-hidden="true">
+                                <span class="likes-counter">${jsonData.likes}</span>
+                              </i>
+                            </a>
                           </div>
                         </div>
                       </div>
